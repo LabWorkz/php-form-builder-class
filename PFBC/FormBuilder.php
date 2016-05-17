b844b98c95fb8587a626620a9154e0963c841c78
Gary Martin <gary@labworkz.com>
2016-05-16 22:41:45 -0500

    Add more fields.

--- PFBC/FormBuilder.php
+++ PFBC/FormBuilder.php
@@ -16,6 +16,17 @@ use PFBC\Element\Checksort;
 use PFBC\Element\CKEditor;
 use PFBC\Element\Color;
 use PFBC\Element\Country;
+use PFBC\Element\Date;
+use PFBC\Element\DateTime;
+use PFBC\Element\DateTimeLocal;
+use PFBC\Element\Email;
+use PFBC\Element\File;
+use PFBC\Element\Hidden;
+use PFBC\Element\HTML;
+use PFBC\Element\jQueryUIDate;
+use PFBC\Element\Month;
+use PFBC\Element\Number;
+use PFBC\Element\Password;
 use PFBC\Element\Select;
 use PFBC\Element\Textbox;
 use PFBC\Form;
@@ -55,6 +66,40 @@ class FormBuilder extends Form
         return $this->addElement(new Country($label, $name,$properties));
     }
 
+    public function date($label, $name, $properties = []){
+        return $this->addElement(new Date($label, $name,$properties));
+    }
+
+    public function dateTime($label, $name, $properties = []){
+        return $this->addElement(new DateTime($label, $name,$properties));
+    }
+    public function dateTimeLocal($label, $name, $properties = []){
+        return $this->addElement(new DateTimeLocal($label, $name, $properties));
+    }
+    public function email($label, $name, $properties = []){
+        return $this->addElement(new Email($label, $name, $properties));
+    }
+    public function file($label, $name, $properties = []){
+        return $this->addElement(new File($label, $name,$properties));
+    }
+    public function hidden($name, $value, $properties = []){
+        return $this->addElement(new Hidden($name,$value, $properties));
+    }
+    public function html($html){
+        return $this->addElement(new HTML($html));
+    }
+    public function jQueryUIDate($label, $name, $properties = []){
+        return $this->addElement(new jQueryUIDate($label, $name, $properties));
+    }
+    public function month($label, $name, $properties = []){
+        return $this->addElement(new  Month($label, $name, $properties));
+    }
+    public function number($label, $name, $properties = []){
+        return $this->addElement(new Number($label, $name, $properties));
+    }
+    public function password($label, $name, $properties = []){
+        return $this->addElement(new Password($label, $name, $properties));
+    }
 
 
     public function textbox($label, $name, $properties = []){
